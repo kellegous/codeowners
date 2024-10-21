5ea9e384098ac46e3cbf93ae786a3687a0ad0e2b
Kelly Norton <kellegous@gmail.com>
2024-10-21 14:58:16 -0400

    adding tests

--- tests/AutomataMatcherTest.php
+++ tests/AutomataMatcherTest.php
@@ -9,24 +9,100 @@ use PHPUnit\Framework\TestCase;
  */
 final class AutomataMatcherTest extends TestCase
 {
-
     /**
      * @return iterable<array{AutomataMatcher, string, ?int}>
      * @throws ParseException
      */
     public static function matchTests(): iterable
     {
-        $matcher = AutomataMatcher::build(
+        $tests = include __DIR__ . '/matcher_tests.php';
+        foreach ($tests as $desc => ['lines' => $lines, 'expected' => $expected]) {
+            $matcher = self::matcherWith($lines);
+            foreach ($expected as $path => $line) {
+                yield "{$desc} w/ {$path}" => [
+                    $matcher,
+                    $path,
+                    $line
+                ];
+            }
+        }
+
+//        yield from self::createTest(
+//            'wildcard overlap w/ literal',
+//            ['/a', '/a/d'],
+//            [
+//                'a/x/d' => 1,
+//                'a/d' => 2,
+//                'a' => 1,
+//            ]
+//        );
+//
+//        yield from self::createTest(
+//            '/a*',
+//            ['/a*'],
+//            [
+//                'b/a' => null,
+//                'a' => 1,
+//                'a1' => 1,
+//                'a2/b' => 1,
+//                'a2/c/d' => 1,
+//            ]
+//        );
+//
+//        yield from self::createTest(
+//            '/*a',
+//            ['/*a'],
+//            [
+//                'b/a' => null,
+//                'a' => 1,
+//                '1a' => 1,
+//                '2a/b' => 1,
+//                '2a/b/c' => 1,
+//            ]
+//        );
+//
+//        yield from self::createTest(
+//            '*/',
+//            ['*/'],
+//            [
+//                'a' => null,
+//                'b/c' => 1,
+//                'b/d/e' => 1,
+//                'c/a' => 1,
+//                'c/b/c' => 1,
+//                'c/b/d/e' => 1,
+//            ]
+//        );
+    }
+
+    /**
+     * @param string[] $lines
+     * @return AutomataMatcher
+     * @throws ParseException
+     */
+    private static function matcherWith(array $lines): AutomataMatcher
+    {
+        return AutomataMatcher::build(
             Owners::fromString(
-                implode(PHP_EOL, ['/a', '/a/d'])
+                implode(PHP_EOL, $lines)
             )->getRules()
         );
-        yield 'example' => [
-            $matcher,
-            'a/x/d',
-            1
-        ];
     }
+
+//    private static function createTest(
+//        string $description,
+//        array $lines,
+//        array $paths
+//    ): iterable {
+//        $matcher = self::matcherWith($lines);
+//        foreach ($paths as $path => $expected) {
+//            yield "{$description} w/ {$path}" => [
+//                $matcher,
+//                $path,
+//                $expected
+//            ];
+//        }
+//    }
 
     /**
      * @param AutomataMatcher $matcher
---
+++ tests/matcher_tests.php
@@ -0,0 +1,94 @@
+<?php
+
+return [
+    'wildcard overlap w/ literal' => [
+        'lines' => ['/a', '/a/d'],
+        'expected' => [
+            'a/x/d' => 1,
+            'a/d' => 2,
+            'a' => 1,
+        ]
+    ],
+
+    '/a*' => [
+        'lines' => ['/a*'],
+        'expected' => [
+            'b/a' => null,
+            'a' => 1,
+            'a1' => 1,
+            'a2/b' => 1,
+            'a2/c/d' => 1,
+        ]
+    ],
+
+    '/*a' => [
+        'lines' => ['/*a'],
+        'expected' => [
+            'b/a' => null,
+            'a' => 1,
+            '1a' => 1,
+            '2a/b' => 1,
+            '2a/b/c' => 1,
+        ]
+    ],
+
+    '*/' => [
+        'lines' => ['*/'],
+        'expected' => [
+            'a' => null,
+            'b/c' => 1,
+            'b/d/e' => 1,
+            'c/a' => 1,
+            'c/b/c' => 1,
+            'c/b/d/e' => 1,
+        ]
+    ],
+
+    '*a/' => [
+        'lines' => ['*a/'],
+        'expected' => [
+            '1a' => null,
+            '2a/b' => 1,
+            '2a/c/d' => 1,
+            'b/1a' => null,
+            'b/2a/b' => 1,
+            'b/2a/c/d' => 1,
+        ]
+    ],
+    'a*/' => [
+        'lines' => ['a*/'],
+        'expected' => [
+            'a1' => null,
+            'a2/b' => 1,
+            'a2/c/d' => 1,
+            'b/a1' => null,
+            'b/a2/b' => 1,
+            'b/a2/c/d' => 1,
+        ]
+    ],
+    '/a/' => [
+        'lines' => ['/a/'],
+        'expected' => [
+            'a' => null,
+            'a/b' => 1,
+            'a/c/d' => 1,
+            'b/a' => null,
+        ]
+    ],
+    '/*/' => [
+        'lines' => ['/*/'],
+        'expected' => [
+            'a' => null,
+            'b/c' => 1,
+            'b/d/e' => 1,
+        ]
+    ],
+    '/*a/' => [
+        'lines' => ['/*a/'],
+        'expected' => [
+            '1a' => null,
+            '2a/b/' => 1,
+            '2a/c/d/' => 1,
+        ]
+    ]
+];
